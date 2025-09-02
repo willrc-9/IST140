ca8d3eefbdfc1a515bf78bad8f3028d521240a1a
Will <37312786+willrc-9@users.noreply.github.com>
2025-09-02 14:49:14 -0400

    Add files via upload

---
+++ MyFirstProgramInJava.java
@@ -0,0 +1,31 @@
+/*
+Comment code
+On multiple lines
+
+ */
+
+
+
+
+
+public class MyFirstProgramInJava {
+    //before the main
+    public static void main(String[] args) {
+        //TODO code application logic here
+        //Print
+        System.out.println("This is a nice day");
+        System.out.println("Enjoy the class");
+        //System.out.print("");
+
+
+        //integer (whole number, no fraction)
+        int x = 2;
+        int y = 5;
+        int z = x+y;
+    }//end of main method
+    /*
+    The main goal of this is to write a multi line comment
+     */
+    //After the main
+}//end of class
+//After the class
